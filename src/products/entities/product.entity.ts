d12ac5bec881cb95bbd6c3cc8b508adffe7bf0ce
JuanFlorez1326 <juanks339@gmail.com>
2022-12-15 19:44:49 -0500

    ADD: Expand the @ApiProperty()

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -6,57 +6,98 @@ import { ApiProperty } from '@nestjs/swagger';
 @Entity({ name: 'products' })
 export class Product {
 
-    @ApiProperty()
+    @ApiProperty({
+        example: '0051d7ae-ef7c-45bb-a0a6-88c8aa9ecd96',
+        description: 'Product ID',
+        uniqueItems: true
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @ApiProperty()
+
+    @ApiProperty({ 
+        example: 'Men’s Chill Crew Neck Sweatshirt',
+        description: 'Product Title',
+        uniqueItems: true
+    })
     @Column('text', {
         unique: true,
     })
     title: string;
 
-    @ApiProperty()
+
+    @ApiProperty({
+        example: 0,
+        description: 'Product Price'
+    })
     @Column('float',{
         default: 0
     })
     price: number;
 
-    @ApiProperty()
+
+    @ApiProperty({
+        example: 'Designed for comfort, the Cybertruck Owl Tee is made from 100% cotton and features our signature Cybertruck icon on the back.',
+        description: 'Product Description',
+        default: null
+    })
     @Column({
         type: 'text',
         nullable: true
     })
     description: string;
 
-    @ApiProperty()
+
+    @ApiProperty({
+        example: 'mens_chill_crew_neck_sweatshirt',
+        description: 'Product Slug for SEO',
+        uniqueItems: true
+    })
     @Column('text', {
         unique: true
     })
     slug: string;
 
-    @ApiProperty()
+
+    @ApiProperty({
+        example: 10,
+        description: 'Product Stock',
+        default: 0
+    })
     @Column('int', {
         default: 0
     })
     stock: number;
 
-    @ApiProperty()
+
+    @ApiProperty({
+        example: ['M','L','XL'],
+        description: 'Product Sizes'
+    })
     @Column('text',{
         array: true
     })
     sizes: string[];
 
-    @ApiProperty()
+
+    @ApiProperty({
+        example: 'men',
+        description: 'Product Gender'
+    })
     @Column('text')
     gender: string;
 
-    @ApiProperty()
+
+    @ApiProperty({
+        example: 'sweatshirt',
+        description: 'Product Category'
+    })
     @Column('text', {
         array: true,
         default: []
     })
     tags: string[];
+
 
     // images
     @ApiProperty()
@@ -66,6 +107,7 @@ export class Product {
         { cascade: true, eager: true }
     )
     images?: ProductImage[];
+
 
     // User
     @ManyToOne(
